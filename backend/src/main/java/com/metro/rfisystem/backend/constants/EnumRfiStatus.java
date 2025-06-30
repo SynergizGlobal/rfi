fc64af0efd7be3950750e2e506c9c10e42ee7605
Administrator <Administrator@DESKTOP-NB4FCDA>
2025-06-30 11:41:42 +0530

    Rfi Status

---
+++ backend/src/main/java/com/metro/rfisystem/backend/constants/EnumRfiStatus.java
@@ -0,0 +1,13 @@
+package com.metro.rfisystem.backend.constants;
+
+public enum EnumRfiStatus {
+	
+	    CREATED,
+	    UPDATED,
+	    RESCHEDULED,
+	    REASSIGNED,
+	    INSPECTED_BY_AE,
+	    VALIDATION_PENDING,
+	    INSPECTION_DONE,
+
+}
