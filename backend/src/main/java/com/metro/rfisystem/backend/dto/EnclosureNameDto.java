3c72884ff0aeb310201df865273b2fd009fbf57d
Mounika V <Mounika V@Synergiz>
2025-09-01 16:47:31 +0530

    EnclosureNameDTO file

---
+++ backend/src/main/java/com/metro/rfisystem/backend/dto/EnclosureNameDto.java
@@ -0,0 +1,13 @@
+package com.metro.rfisystem.backend.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class EnclosureNameDto {
+    private Long id;
+    private String encloserName;
+}
